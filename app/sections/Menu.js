2e873928715b0cb6806a9d2b5539b3a17b8d3960
majorazero <daniel.cw.hsu@gmail.com>
2019-01-16 17:15:45 -0800

    added buttons

---
+++ app/sections/Menu.js
@@ -0,0 +1,68 @@
+import React, {Component} from "react";
+import {StyleSheet, View, TouchableOpacity, Text, Alert} from "react-native";
+
+class Menu extends Component {
+  onPress = () => {
+    Alert.alert("You tapped the button!");
+  }
+  render(){
+    return(
+      <View style={styles.container}>
+        <View style={styles.buttonRow}>
+          <TouchableOpacity style={styles.buttonStyles} onPress={this.onPress}>
+            <Text>LESSONS</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonStyles} onPress={this.onPress}>
+            <Text>REGISTER</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.buttonRow}>
+          <TouchableOpacity style={styles.buttonStyles} onPress={this.onPress}>
+            <Text>BLOG</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonStyles} onPress={this.onPress}>
+            <Text>CONTACT</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.buttonRow}>
+          <TouchableOpacity style={styles.buttonStyles} onPress={this.onPress}>
+            <Text>QUIZ</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonStyles} onPress={this.onPress}>
+            <Text>ABOUT</Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container:{
+      flex: 6,
+      backgroundColor: '#35605a'
+    },
+    buttonRow: {
+      flex: 2,
+      flexDirection: "row",
+      alignItems: "center",
+      borderColor: "#ffffff",
+      borderBottomWidth: 1
+    },
+    buttonStyles: {
+      backgroundColor: "#35606a",
+      width: '50%',
+      height: "50%",
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    buttonText: {
+      color:"#ffffff",
+      fontSize: 18
+    }
+});
+
+export default Menu;
--- app/views/Home.js
+++ app/views/Home.js
@@ -2,6 +2,7 @@ import React,{Component} from "react";
 import {StyleSheet, Text, View } from "react-native";
 import Header from "../sections/Header.js";
 import Hero from "../sections/Hero.js";
+import Menu from "../sections/Menu.js";
 
 class Home extends Component {
   render(){
@@ -9,8 +10,7 @@ class Home extends Component {
       <View style={styles.container}>
         <Header message={'Press to login'} />
         <Hero />
-        <Text style={{flex:8}}>This is a homepage.</Text>
-        <Text style={{flex:6}}>Is this working?</Text>
+        <Menu />
       </View>
     );
   }
